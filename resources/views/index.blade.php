f5a62b5fd8d2e36f67170579d461cd8f596f8ee1
naganao <32755305+naganao@users.noreply.github.com>
2018-08-09 16:45:59 +0900

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'nagablog')
+
+@section('content')
+<div>
+  hello
+</div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>@yield('title')</title>
+  <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+</head>
+<body>
+  @yield('content')
+</body>
+</html>
